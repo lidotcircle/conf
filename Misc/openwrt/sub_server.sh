9ebb99c890717868ecd3291207569abbf9b25ca9
analyman <beam.analyman@gmail.com>
2020-01-03 21:27:08 +0800

    asdfzxcv

--- Misc/openwrt/sub_server.sh
+++ Misc/openwrt/sub_server.sh
@@ -1,9 +1,41 @@
 #!/bin/bash
 
-# dependencies check
-[ -z $(which curl) ] && echo "need curl" && exit 1
-[ -z $(which uci)  ] && echo "need uci" && exit 1
+# 0.     check prerequisite software.
+# 1.     if network is accessible, then nothing will be do.
+# 2.     otherwise.
+# 2.1.     try to fetch subscription.
+# 2.1.1.     if fetch subscription fail, using previous cache
+# 2.1.2.     if previous cache isn't avaliable, kill this process
+# 2.2.     updating shadowsocksr files by decoding subscription file
+# 2.3.     if existing a shadowsocksr process, kill it
+# 2.4.     restart shadowsockrsr process with new server, 
+#          and redo this step until can connect to <https times://google.com> with 
+#          interval of 20 seconds and maxmimum trying of 5 times.
 
+#{ logger
+__logger__()
+{
+    local MSG
+    local PRIORITY
+    if [ $# -eq 1 ]; then
+        MSG="$1"
+        PRIORITY="INFO"
+    fi
+    PRIORITY="$1"
+    MSG="$2"
+    if [ "${PRIORITY}" == "ERROR" ]; then
+        echo -e "ERROR [$(date)]: $MSG" 1>&2
+        return 0
+    else
+        echo -e "${PRIORITY} [$(date)]: $MSG"
+    fi
+} #}
+
+# dependencies check
+[ -z $(which curl) ] && __logger__ "ERROR" "need curl" && exit 1
+[ -z $(which uci)  ] && __logger__ "ERROR" "need uci"  && exit 1
+
+declare -r SSR_SUB_DIR="/etc/ssr_fold__"
 declare -r SSR_SUB=$(uci get ssr_json.@ssrsub[0].suburl)
 declare -r SUB_FILE=$(uci get ssr_json.@ssrsub[0].subfile)
 if [ -d ${SUB_FILE%/*} ]; then
@@ -48,6 +80,18 @@ if [ $NETCAT_AVAIL -eq 1 ]; then
 fi
 done
 
+ #{ function: clean_exit(exit_status)
+clean_exit()
+{
+    [ -f "${TMP_FILE}"  ]    && rm -f ${TMP_FILE}
+    [ -f "${TMP_FILEB}" ]    && rm -f ${TMP_FILEB}
+    [ -f "${SUB_FILE}"  ]    && rm -f ${SUB_FILE}
+    [ -f "${SUB_FILE_DST}" ] && rm -f ${SUB_FILE_DST}
+    [ "${START_BANNER}" == 1] && __logger__ "INFO" "----------------- END ------------------\n"
+    [ $# -eq 1 ] && return $1
+    return 1
+} #}
+
 #{ function: get_fd() <ret_ref> -- aviliable file descriptor
 get_fd()
 {
@@ -70,24 +114,24 @@ test_ssr_server()
     # just one argument passed
     if [ $# -eq 1 ]; then
         [ $PING_AVAIL -eq 1 ] && ping -w 5 -c 2 $1 || \
-            (echo -e "### CAN'T access server <$1> ###\n" && return 1)
+            (__logger__ "WARNING" "CAN'T access server <$1>\n" && return 1)
         return 0
     fi
     if [ $# -eq 2 ]; then
         if [ $NMAP_AVAIL -eq 1 ]; then
-           nmap --host-timeout 2 $1 -p $2 2>/dev/null | grep -ie "OPEN" 1>/dev/null 2>&1 && return 0
-           echo -e "### CAN'T access server <$1:$2> ###\n" >&2 && return 1
+            nmap --host-timeout 2 $1 -p $2 2>/dev/null | grep -ie "OPEN" 1>/dev/null 2>&1 && return 0
+            __logger__ "WARNING" "CAN'T access server <$1:$2>\n" >&2 && return 1
         fi
         if [ $NETCAT_AVAIL -eq 1 ]; then
             $NETCAT_CMD -zvw2 $1 $2 1>/dev/null 2>&1 && return 0
-            echo -e "### CAN'T access server <$1:$2> ###\n" >&2 && return 1
+            __logger__ "WARNING" "CAN'T access server <$1:$2>\n" >&2 && return 1
         fi
         if [ $PING_AVAIL -eq 1 ]; then
             ping -w 5 -c 2 $1 1>/dev/null 2>&1 && return 0
-            echo -e "### CAN'T access server <$1> ###\n" >&2 && return 1
+            __logger__ "WARNING" "CAN'T access server <$1>\n" >&2 && return 1
         fi
     fi
-    echo -e "### unable to test accessibility of server, just return true." && return 0
+    __logger__ "INFO" "unable to test accessibility of server, just return true." && return 0
 }
 #}
 
@@ -101,11 +145,10 @@ add_node()
     local first_part=$(cat ${TMP_FILEB} | cut -f 1 -d\/)
     local server=$(echo "${first_part}" | cut -f 1 -d:)
     local server_port=$(echo "${first_part}" | cut -f 2 -d:)
-    echo "${server}"
     test_ssr_server "${server}" "${server_port}" || return 1
-    echo "### adding node <${server}> ###"
     local second_part=$(cat ${TMP_FILEB} | cut -f 2 -d?)
-    eval "declare -g -A ssr_node_${ssr_node_i}"
+    local -r declaration=$(eval "declare -g -A ssr_node_${ssr_node_i}")
+    __logger__ "INFO" "---------- Add Node ---------\n ${declaration}"
     declare -n tmp_ref=ssr_node_${ssr_node_i}
     # First part
     tmp_ref["server"]=$server
@@ -131,12 +174,10 @@ add_node()
         if [ $? -eq 0 ] || [ $para_name == "obfsparam" ] || [ $para_name == "protoparam" ]; then
             para_cont=$(echo $para_cont | base64 -d 2>/dev/null)
         fi
-        echo "$para_name"
         eval "tmp_ref[\"${para_name}\"]=\"${para_cont}\""
     done
     declare -p ssr_node_${ssr_node_i}
     let ssr_node_i++
-    echo -e "############################################\n\n"
 }
 #}
 
@@ -183,24 +224,76 @@ luci_ssrpro_config()
 }
 #}
 
+#{ function: write_to_fold() -- write nodes into files in specified fold
+write_to_fold()
+{
+    [ ! $# -eq 1 ] && return 1
+    if [ ! -d "$1" ]; then
+        if [ ! -e $1 ]; then
+            mkdir -p $1
+        fi
+        rm -f $1
+        mkdir -p $1
+    else
+        rm -rf "$1"
+        mkdir $1
+    fi
+
+    for ((i=1; i<=$ssr_node_i; ++i)); do
+        print_json "$i" "$1/json_$i"
+        luci_ssrpro_config "$i" "$1/conf_$i"
+    done
+} #}
+
+#{ function: start_ssr_process() -- as the function name suggests
+start_ssr_process()
+{
+    [ ! $# -eq 1 ] && __logger__ "ERROR" "Invaliated Argument at FUNCTION start_ssr_process()" && clean_exit 1
+
+    [ -f ${SSR_CONF_JSON} ] && rm -f ${SSR_CONF_JSON} && \
+        __logger__ "INFO" "remove ssr json config file -- ${SSR_CONF_JSON}"
+    [ -f ${LUCI_SSRPRO}   ] && rm -f ${LUCI_SSRPRO}   && \
+        __logger__ "INFO" "remove luci config file -- ${LUCI_SSRPRO}"
+
+    cp -f "${SSR_SUB_DIR}/json_$1" ${SSR_CONF_JSON}
+    cp -f "${SSR_SUB_DIR}/conf_$1" ${LUCI_SSRPRO}
+    __logger__ "INFO" "To start ssr with server $1"
+    ${SSR_START} && __logger__ "INFO" "start ssr success."
+    return 0
+}
+#}
+
+#{ function: kill_ssr_process()
+kill_ssr_process()
+{
+    local -i ssrpid=0
+    [ -f ${SSR_PID} ] && rm -rf ${SSR_PID}
+    while (true); do
+        ssrpid="$(ps | grep "ssr" | grep -v "grep" | tail -n 1 | cut -f 1)"
+        if [ $ssrpid -eq 0 ]; then
+            return 0
+        fi
+        kill -9 $ssrpid
+        __logger__ "INFO" "kill shadowsocksr process #${ssrpid} ###"
+    done
+}
+#}
+
 #{ function: restart_ssr()
 RANDOM=$$
 restart_ssr()
 {
-    local ssrpid
-    if [ -f ${SSR_PID} ]; then
-        ssrpid=$(cat ${SSR_PID})
-    else
-        ssrpid=$(${PS} | grep -se "ssr" | tail -n 1 | cut -f 1)
-    fi
-    [ -n ${ssrpid} ] && kill -9 ${ssrpid} && echo -e "### kill ssr process at <$(date)> ###"
-    [ -f ${SSR_CONF_JSON} ] && rm -f ${SSR_CONF_JSON} && \
-        echo "remove ssr config file -- ${SSR_CONF_JSON}"
-    local rand=$(($RANDOM%$ssr_node_i))
+    # amount of server that is available
+    declare -ri count_of_server=$(ls ${SSR_SUB_DIR} | wc -l)
+    [ ${count_of_server} -eq 0 ] && clean_exit 1
+    __logger__ "INFO" "total amount of server that is available is ${count_of_server}"
+
+    kill_ssr_process
+
+    local rand=$(($RANDOM%$count_of_server))
     [ -n "$1" ] && rand=$1
-    rm -f ${LUCI_SSRPRO}; luci_ssrpro_config "${rand}" "${LUCI_SSRPRO}"
-    print_json "${rand}" "${SSR_CONF_JSON}"
-    echo "restart ssr." && ${SSR_START} && echo "start ssr success."
+
+    start_ssr_process "${rand}"
 }
 #}
 
@@ -208,21 +301,25 @@ restart_ssr()
 check_accessibility()
 {
     curl --max-time 5 https://google.com 1>/dev/null 2>&1 || \
-        (echo "Can't access google... [$(date)]" && return 1) && \
-        (echo "Google is accessible... [$(date)]" && return 0)
+        (__logger__ "INFO" "Can't access google... " && return 1) && \
+        (__logger__ "INFO" "Google is accessible..." && return 0)
 }
 #}
 
 # banner
-echo -e "\n########## [$(date)] ##########\n"
+__logger__ "INFO" "\n--------------- START -------------------"
+declare -r START_BANNER="1"
 
-check_accessibility && exit 0
+check_accessibility && clean_exit 0
 
 # get the subscription file from URL
+declare -i SUBSCRIPTION_SUCCESS=1
 curl --max-time 5 ${SSR_SUB} -o ${TMP_FILE} 1>>/dev/null 2>&1 && cp -f ${TMP_FILE} ${SUB_FILE} \
     || ([ -f ${SUB_FILE}] && cp -f ${SUB_FILE} ${TMP_FILE})
-[ ! -f $SUB_FILE ] && echo "subscription fail!!!"  && exit 1
+[ ! -f $SUB_FILE ] && __logger__ "WARNING" "subscription fail!!!"  && SUBSCRIPTION_SUCCESS=0
 
+
+while [ $SUBSCRIPTION_SUCCESS -eq 1]; do
 # decode subscription file
 base64 -d ${TMP_FILE} > ${SUB_FILE_DST} 2>/dev/null
 cat ${SUB_FILE_DST} | grep -oe "[^:^/]\{4,\}" > ${TMP_FILE}
@@ -234,8 +331,17 @@ for ((node_loop=1;$node_loop<=$node_all_i;node_loop++)); do
     add_node "${__node}" "${SUB_FILE_DST}"
 done
 
-declare -i max_try=10
+# write write write ...
+write_to_fold "${SSR_SUB_DIR}"
+
+break # while [ $SUBSCRIPTION_SUCCESS -eq 1];
+done
+
+
+declare -i max_try=5
 for((i=0;$i<$max_try;i++)); do
-    check_accessibility && exit 0 || restart_ssr
+    check_accessibility && clean_exit 0 || restart_ssr
     sleep 20
 done
+
+clean_exit 1
