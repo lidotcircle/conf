decce8436257d12a7eb7a306fcf66c7e4200a8bf
lidotcircle <beam.analyman@gmail.com>
2024-08-11 10:10:29 +0000

    better debugging (DAP and keymap)

--- config/nvim/lua/hula/plugins/init.lua
+++ config/nvim/lua/hula/plugins/init.lua
@@ -73,19 +73,31 @@ use { 'hrsh7th/nvim-cmp', config = function()
     })
 end
 }
-use 'sakhnik/nvim-gdb'
 use {
-    'mfussenegger/nvim-dap',
+    'sakhnik/nvim-gdb',
     config = function()
-        nnoremap("<leader>lc", "<Cmd>lua require'dap'.continue()<CR>")
-        nnoremap("<leader>ln", "<Cmd>lua require'dap'.step_over()<CR>")
-        nnoremap("<leader>ls", "<Cmd>lua require'dap'.step_into()<CR>")
-        nnoremap("<leader>lo", "<Cmd>lua require'dap'.step_out()<CR>")
-        nnoremap("<Leader>lb", "<Cmd>lua require'dap'.toggle_breakpoint()<CR>")
-        nnoremap("<Leader>lB", "<Cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
-        nnoremap("<Leader>lp", "<Cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
-        nnoremap("<Leader>dr", "<Cmd>lua require'dap'.repl.open()<CR>")
-        nnoremap("<Leader>dl", "<Cmd>lua require'dap'.run_last()<CR>")
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+            callback = function(ev)
+                if type(ev.match) == "string" and string.match(ev.match, "nvimgdb") then
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>n", "<Cmd>GdbNext<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>s", "<Cmd>GdbStep<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>o", "<Cmd>GdbFinish<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>c", "<Cmd>GdbContinue<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>t", "<Cmd>GdbDebugStop<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>p", "<Cmd>GdbInterrupt<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>u", "<Cmd>GdbFrameUp<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>d", "<Cmd>GdbFrameDown<CR>",
+                        { noremap = true, silent = true })
+                end
+            end
+        })
     end
 }
 use {
@@ -112,6 +124,15 @@ use {
                     }
                 }
             end,
+            ["clangd"] = function()
+                require("lspconfig").clangd.setup {
+                    on_attach = lsp_on_attach,
+                    cmd = {
+                        "clangd",
+                        "--offset-encoding=utf-16",
+                    },
+                }
+            end,
             ["lua_ls"] = function()
                 require("lspconfig").lua_ls.setup {
                     on_attach = lsp_on_attach,
@@ -129,18 +150,73 @@ use {
         require('monokai').setup { palette = require('monokai').pro }
     end
 }
-use 'mfussenegger/nvim-dap-python'
 use {
     'mfussenegger/nvim-dap',
     config = function()
         local wk = require("which-key")
         wk.register({
             d = {
-                a = { function() require("dap").continue() end, "DAP Debug" }
+                a = { function() require("dap").continue() end, "DAP Debug" },
+                b = { function() require("dap").toggle_breakpoint() end, "DAP Toggle Breakpoint" },
+                l = { function() require("dap").run_last() end, "DAP Run Last" },
+                c = { function() require("dap").run_to_cursor() end, "DAP Run until cursor" },
             }
         }, { prefix = "<leader>" })
+
+        local dap = require('dap')
+        dap.adapters.gdb = {
+            id = 'gdb',
+            type = 'executable',
+            command = 'gdb',
+            args = { '--quiet', '--interpreter=dap' },
+        }
+        dap.configurations.c = {
+            {
+                name = 'Run executable (GDB)',
+                type = 'gdb',
+                request = 'launch',
+                program = function()
+                    local path = vim.fn.input({
+                        prompt = 'Path to executable: ',
+                        default = vim.fn.getcwd() .. '/',
+                        completion = 'file',
+                    })
+
+                    return (path and path ~= '') and path or dap.ABORT
+                end,
+            },
+            {
+                name = 'Run executable with arguments (GDB)',
+                type = 'gdb',
+                request = 'launch',
+                program = function()
+                    local path = vim.fn.input({
+                        prompt = 'Path to executable: ',
+                        default = vim.fn.getcwd() .. '/',
+                        completion = 'file',
+                    })
+
+                    return (path and path ~= '') and path or dap.ABORT
+                end,
+                args = function()
+                    local args_str = vim.fn.input({
+                        prompt = 'Arguments: ',
+                    })
+                    return vim.split(args_str, ' +')
+                end,
+            },
+            {
+                name = 'Attach to process (GDB)',
+                type = 'gdb',
+                request = 'attach',
+                processId = require('dap.utils').pick_process,
+            },
+        }
+        dap.configurations.cpp = dap.configurations.c
     end
 }
+use { 'jay-babu/mason-nvim-dap.nvim' }
+use 'mfussenegger/nvim-dap-python'
 use {
     'leoluz/nvim-dap-go',
     config = function()
@@ -163,7 +239,54 @@ use {
         }
     end
 }
-use 'rcarriga/nvim-dap-ui'
+use {
+    'rcarriga/nvim-dap-ui',
+    config = function()
+        local dap = require("dap")
+        local dapui = require("dapui")
+        dapui.setup()
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close()
+        end
+
+        local wk = require("which-key")
+        wk.register({
+            d = {
+                u = { function() require("dapui").toggle() end, "DAP UI Toggle" },
+            }
+        }, { prefix = "<leader>" })
+
+        vim.api.nvim_create_autocmd({ "FileType" }, {
+            callback = function(ev)
+                if type(ev.match) == "string" and string.match(ev.match, "dapui*") then
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>n", "<Cmd>lua require'dap'.step_over()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>s", "<Cmd>lua require'dap'.step_into()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>o", "<Cmd>lua require'dap'.step_out()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>c", "<Cmd>lua require'dap'.continue()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>t",
+                        "<Cmd>lua require'dap'.close(); require('dapui').close()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>p", "<Cmd>lua require'dap'.pause()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>u", "<Cmd>lua require'dap'.up()<CR>",
+                        { noremap = true, silent = true })
+                    vim.api.nvim_buf_set_keymap(ev.buf, "n", "<space>d", "<Cmd>lua require'dap'.down()<CR>",
+                        { noremap = true, silent = true })
+                end
+            end
+        })
+    end
+}
 use {
     'nvim-treesitter/nvim-treesitter',
     config = function()
@@ -204,6 +327,12 @@ use {
     config = function()
         require('telescope').load_extension("recent_files")
         nnoremap("<Leader><Leader>", "<cmd>lua require('telescope').extensions.recent_files.pick()<CR>")
+    end
+}
+use {
+    "nvim-telescope/telescope-dap.nvim",
+    config = function()
+        require("telescope").load_extension("dap")
     end
 }
 use {
